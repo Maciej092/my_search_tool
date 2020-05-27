2f1f688798d95e96222133490883505c5eeacece
macius092 <maciej.skorski092@gmail.com>
2020-05-27 23:53:34 +0200

    Final commit, update scrapper of gumtree core

--- my_app/views.py
+++ my_app/views.py
@@ -5,8 +5,9 @@ from requests.compat import quote_plus
 from . import models
 
 
-BASE_URL     = 'https://www.olx.pl/oferty/q-{}/'
-BASE_IMG_URL = 'https://ireland.apollo.olxcdn.com/v1/files/{}-PL/image;s=644x461'
+BASE = 'https://www.gumtree.pl{}'
+BASE_URL     = 'https://www.gumtree.pl/s-krakow/v1l3200208p1?q={}'
+BASE_IMG_URL = 'https://i.ebayimg.com/images/g/{}/s-l140.jpg'
 
 
 def home(request):
@@ -19,25 +20,28 @@ def new_search(request):
     final_url = BASE_URL.format(quote_plus(search))
     response  = requests.get(final_url)
     data      = response.text
+    print(data)
     soup      = BeautifulSoup(data, features='html.parser')
 
-    post_listings = soup.find_all('li', {'class': 'result-row'})
-
+    post_listings = soup.find_all('div', {'class': 'tileV1'})
+    print(post_listings)
     final_postings = []
 
     for post in post_listings:
 
-        post_titles = post.find(class_='result-title').text
+        post_titles = post.find(class_='tile-title-text').text
         post_url    = post.find('a').get('href')
-
-        if post.find(class_='result-price'):
-            post_price  = post.find(class_='result-price').text
+        post_url    = BASE.format(post_url)
+        print(post_titles)
+        print('======================')
+        print(post_url)
+        if post.find(class_='ad-price'):
+            post_price  = post.find(class_='ad-price').text
         else:
             post_price  = 'N/A'
 
-        if post.find(class_='result-image').get('data-ids'):
-            post_image_id  = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
-            post_image_url = BASE_IMG_URL.format(post_image_id)
+        if post.img:
+            post_image_url = post.img['data-src']
         else:
             post_image_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1200px-No_image_available.svg.png'
 
